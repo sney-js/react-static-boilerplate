11fdc200d52c1d33043be482e3fae17f09890756
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2020-02-19 13:10:12 +0000

    header wip

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -3,6 +3,7 @@ import { generateClassList } from "../../utils/helpers";
 import Container from "../container/Container";
 import LinkWrap from "../../containers/link/linkWrap";
 import { RespImage } from "../../utils/RespImage";
+import LinkElement from "../../elements/link/LinkElement";
 
 const styles = require("./header.module.scss");
 
@@ -32,17 +33,23 @@ export class Header extends React.Component<HeaderProps, HeaderState> {
     headerRef: any;
 
     render() {
-        console.log(this.props.logo);
+        console.log("HEADER", this.props);
         return (
             <header ref={this.headerRef} className={generateClassList([styles.header])}>
-                <div className={styles.logo} >
-                    <RespImage image={this.props.logo} width={"300px"}/>
-                </div>
-                <div className={styles.navigation} >
-                    {this.props.links.map(l => {
-                        return <LinkWrap {...l} />;
-                    })}
-                </div>
+                <Container maxWidth padded_x>
+                    <div className={styles.container}>
+                        <div className={styles.logo}>
+                            <LinkElement path={"/"}>
+                                <RespImage image={this.props.logo} width={"100px"} />
+                            </LinkElement>
+                        </div>
+                        <div className={styles.navigation}>
+                            {this.props.links.map(l => {
+                                return <LinkWrap {...l} />;
+                            })}
+                        </div>
+                    </div>
+                </Container>
             </header>
         );
     }
