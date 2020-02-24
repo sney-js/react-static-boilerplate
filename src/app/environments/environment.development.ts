118c3ae512b96535ba7bb5c1e045b76c3a05c023
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2020-02-24 12:13:46 +0000

    freebees generic++

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     localStorageKey: "d-data",
-    apiUrl: "https://dev.freebees.nl/api",
+    apiUrl: "/",
     contentful: {
         space: "xyyr4mk4uku2",
         accessToken: "0UYIUDEai2pMj0Nfre0R00WWUC2nt37uU3vTiLCuUDM",
--- src/app/environments/environment.production.ts
+++ src/app/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     localStorageKey: "d-data",
-    apiUrl: "https://prod.freebees.nl/api",
+    apiUrl: "/",
 };
