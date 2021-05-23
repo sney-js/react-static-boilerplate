2fa5849a47df3af469d28fcf6c001854294b7551
Snehil Bhushan <virtualrealityneil@gmail.com>
2021-05-23 19:29:33 +0100

    feat(storybook): Bug fix and upgrade with controls

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { getLocalIdent } = require("css-loader/dist/utils");
 
 const prodFilesGlob = [
     "../src/**/!(development)/*.stories.(js|ts|tsx|jsx|mdx)",
@@ -10,18 +11,85 @@ const devFilesGlob = [
     "./**/*.stories.(js|ts|tsx|jsx|mdx)",
 ];
 
+const styleLoader = {
+    loader: require.resolve("style-loader"),
+    options: {
+        hmr: false,
+    },
+};
+const cssLoader = {
+    loader: require.resolve("css-loader"),
+    options: {
+        modules: true,
+        camelCase: true,
+        importLoaders: 2,
+        localIdentName: "[name]-[local]_[hash:base64:4]",
+        getLocalIdent: (context, localIdentName, localName, options) =>
+            context.resourcePath.includes("/node_modules/")
+                ? localName
+                : getLocalIdent(context, localIdentName, localName, options),
+    },
+};
+const postcssLoader = {
+    loader: require.resolve("postcss-loader"),
+    options: {
+        plugins: [
+            require("postcss-flexbugs-fixes"),
+            require("autoprefixer")({
+                flexbox: "no-2009",
+            }),
+        ],
+    },
+};
+const sassLoader = {
+    loader: require.resolve("sass-loader"),
+    options: {
+        includePaths: [
+            path.resolve(__dirname, "../src"),
+            path.resolve(__dirname, "../src/app/scss"),
+        ],
+        javascriptEnabled: true,
+    },
+};
+
 module.exports = {
     stories: process.env.NODE_ENV === "prod" ? prodFilesGlob : devFilesGlob,
-    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+    addons: [
+        "@storybook/addon-links",
+        {
+            name: "@storybook/addon-docs",
+            options: {
+                configureJSX: true,
+                transcludeMarkdown: true,
+            },
+        },
+        "@storybook/addon-essentials",
+        "storybook-addon-themes",
+        "@storybook/addon-jest",
+        "@storybook/addon-a11y",
+    ],
     webpackFinal: (config) => {
         config.resolve.modules.push(process.cwd() + "/node_modules");
 
         // this is needed for working w/ linked folders
         config.resolve.symlinks = false;
         config.module.rules.push({
-            test: /\.scss$/,
-            use: ["style-loader", "css-loader", "sass-loader"],
+            test: /\.module.scss$/,
+            use: [styleLoader, cssLoader, postcssLoader, sassLoader],
+        });
+
+        config.module.rules.push({
+            test: /\.s(a|c)ss$/,
+            exclude: /\.module.scss$/,
             include: path.resolve(__dirname, "../"),
+            use: [
+                // Creates `style` nodes from JS strings
+                styleLoader,
+                // Translates CSS into CommonJS
+                "css-loader",
+                // Compiles Sass to CSS
+                sassLoader,
+            ],
         });
         return config;
     },
---
+++ src/app/components/carousel/carousel.stories.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { Carousel, CarouselProps } from "./Carousel";
+import Header from "../header/Header";
+import { Meta } from "@storybook/react/types-6-0";
+
+export default {
+    title: "Components/Carousel",
+    component: Header,
+} as Meta;
+
+export const basic = (args: CarouselProps): React.ReactElement => {
+    return <Carousel {...args} />;
+};
+
+basic.args = {
+    items: [
+        <img src="https://dummyimage.com/600x400/000/fff.jpg" />,
+        <img src="https://dummyimage.com/1000x500/d435d4/fff.jpg" />,
+        <img src="https://dummyimage.com/1600x400/000/fff.jpg" />,
+    ],
+};
